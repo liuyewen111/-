a27a7f6500ff12f7c8c87322a8ad5be97d30ee00
liuyewen111 <liuywewen111@gmail.com>
2024-12-17 12:44:56 +0800

    Update uptime.config.ts

--- uptime.config.ts
+++ uptime.config.ts
@@ -1,12 +1,10 @@
 const pageConfig = {
   // Title for your status page
-  title: "AM科技's Status Page",
+  title: "MH Blog's Status Page",
   // Links shown at the header of your status page, could set `highlight` to `true`
   links: [
-    { link: 'https://am.809098.xyz', label: '个人博客', highlight: true },
-    { link: 'https://youtube.com/@AM_CLUB', label: 'AM科技' },
-    { link: 'https://github.com/amclubs', label: 'GitHub' },
-    { link: 'https://809098.xyz', label: 'Blog'},
+    { link: 'https://blog.280222.xyz', label: '个人博客', highlight: true },
+    { link: 'https://space.bilibili.com/1775420054?spm_id_from=333.1007.0.0', label: '米花街的狂奔' },
   ],
 }
 
@@ -18,46 +16,16 @@ const workerConfig = {
   // Define all your monitors here
   monitors: [
     // Example HTTP Monitor
-    {
-      // `id` should be unique, history will be kept if the `id` remains constant
-      id: 'am.809098.xyz',
-      // `name` is used at status page and callback message
-      name: '个人博客',
-      // `method` should be a valid HTTP Method
-      method: 'GET',
-      // `target` is a valid URL
-      target: 'https://am.809098.xyz',
-      // [OPTIONAL] `tooltip` is ONLY used at status page to show a tooltip
-      tooltip: 'This is a tooltip for this monitor',
-      // [OPTIONAL] `statusPageLink` is ONLY used for clickable link at status page
-      statusPageLink: 'https://am.809098.xyz',
-      // [OPTIONAL] `expectedCodes` is an array of acceptable HTTP response codes, if not specified, default to 2xx
-      // expectedCodes: [200],
-      // [OPTIONAL] `timeout` in millisecond, if not specified, default to 10000
-      timeout: 10000,
-      // [OPTIONAL] headers to be sent
-      // headers: {
-      //   'User-Agent': 'Uptimeflare',
-      //   Authorization: 'Bearer YOUR_TOKEN_HERE',
-      // },
-      // [OPTIONAL] body to be sent
-      // body: 'Hello, world!',
-      // [OPTIONAL] if specified, the response must contains the keyword to be considered as operational.
-      // responseKeyword: 'success',
-      // [OPTIONAL] if specified, the check will run in your specified region,
-      // refer to docs https://github.com/lyc8503/UptimeFlare/wiki/Geo-specific-checks-setup before setting this value
-      // checkLocationWorkerRoute: 'https://am.809098.xyz',
-    },
     // Example TCP Monitor
     {
-      id: '809098.xyz',
+      id: 'blog.280222.xyz',
       name: 'Blog',
       // `method` should be `TCP_PING` for tcp monitors
       method: 'GET',
       // `target` should be `host:port` for tcp monitors
-      target: 'https://809098.xyz',
+      target: 'https://blog.280222.xyz',
       tooltip: 'My production server monitor',
-      statusPageLink: 'https://809098.xyz',
+      statusPageLink: 'https://blog.280222.xyz',
       timeout: 10000,
     },
   ],
